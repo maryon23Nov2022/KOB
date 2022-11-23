042fdaed865989e61991e2a17eaf293eec9509b8
mary <qingbuzhu5@gmail.com>
2022-11-23 17:49:34 +0800

    backend completed

--- Backend/src/main/java/com/example/backend/controller/pk/BotInfoController.java
+++ Backend/src/main/java/com/example/backend/controller/pk/BotInfoController.java
@@ -1,13 +1,23 @@
+//Separating front and back ends.
+
 package com.example.backend.controller.pk;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController{
     @RequestMapping("getbotinfo/")
-    public String getbotinfo(){
-        return "skdflj";
+//http://127.0.0.1:8080/pk/getbotinfo/
+    public Map<String, String> getbotinfo(){
+        Map<String, String> bot = new HashMap<>();
+        bot.put("name", "mary");
+        bot.put("rating", "inf");
+        return bot;
     }
 }
--- Backend/src/main/java/com/example/backend/controller/pk/IndexController.java
+++ Backend/src/main/java/com/example/backend/controller/pk/IndexController.java
@@ -1,3 +1,5 @@
+//No separation between front and back end.
+
 package com.example.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/pk")
 public class IndexController {
     @RequestMapping("index/")
+//http://127.0.0.1:8080/pk/index/
     public String inded(){
         return "pk/index.html";
     }
